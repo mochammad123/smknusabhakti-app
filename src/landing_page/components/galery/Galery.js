76073069d535690f0167d76b8f5604f879fdaf0a
Moch. Faisal Ediansyah <faisalmochammad54@gmail.com>
2023-02-13 14:48:29 +0700

    Updated

--- src/landing_page/components/galery/Galery.js
+++ src/landing_page/components/galery/Galery.js
@@ -43,33 +43,35 @@ const Galery = () => {
           <div className="row">
             <Header title="Galeri" />
 
-            <div className="flex justify-center sm:justify-end mt-10">
-              <button
-                className="btn mr-5 bg-green-300 hover:bg-green-400 border-none"
-                onClick={() => getAllGaleries()}
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-6 h-6 text-white"
+            <div className="flex-none lg:flex md:flex justify-center sm:justify-end mt-10">
+              <div className="flex justify-end">
+                <button
+                  className="btn mr-0 sm:mr-5 mb-5 sm:mb-0 bg-green-300 hover:bg-green-400 border-none"
+                  onClick={() => getAllGaleries()}
                 >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"
-                  />
-                </svg>
-              </button>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6 text-white"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"
+                    />
+                  </svg>
+                </button>
+              </div>
               <form onSubmit={handleSearch}>
                 <div className="form-control">
                   <div className="input-group">
                     <input
                       type="text"
                       placeholder="Search…"
-                      className="input input-bordered input-info bg-white"
+                      className="input input-bordered input-info bg-white w-full"
                       value={keyword}
                       onChange={handleChangeKeyword}
                     />
